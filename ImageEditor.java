f60bba603d6b4c44bd46c3c9195cb2407c6cce13
Christoph- <christophmennerich@yahoo.de>
2015-05-07 11:23:17 +0200

    leeres frame

--- ImageEditor.java
+++ ImageEditor.java
@@ -4,11 +4,15 @@ import java.awt.*;
 import java.awt.image.*; 
 import java.io.*; 
 import javax.imageio.*; 
-public class ImageEditorFrame extends JFrame
-{ public ImageEditorFrame()
-  { 
-  setDefaultCloseOperation(EXIT_ON_CLOSE);
-  setSize(400, 300);
-  setVisible(true);
-  }
+public class ImageEditorFrame extends JFrame{ 
+
+	public ImageEditorFrame(){ 
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(400, 300);
+		setVisible(true);
+	}
+
+	public static void main(String[] args){
+		ImageEditorFrame frame = new ImageEditorFrame();
+	}
 }
